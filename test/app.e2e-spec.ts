61ff801a2ec0bd3ef47469b92f183c20af994134
george <g.okiri79@gmail.com>
2023-02-03 14:09:55 +0300

    Test cases

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { TransactionController } from './../src/transaction/transaction.controller';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
